e82e1be0951686dbb185fd9ccd4bebbf3b4aaaa5
Brian Plunkett <brian.plunkett@gmail.com>
2016-07-06 13:14:36 -0500

    Create manipulating-complex-objects.js

---
+++ freecodecamp/manipulating-complex-objects.js
@@ -0,0 +1,29 @@
+//Instructions
+//Add a new album to the myMusic object. Add artist and title strings, release_year number, and a formats array of strings.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true    
+  },
+  
+  {
+    "artist": "The Smashing Pumpkins",
+    "title": "Rotten Apples",
+    "release_year": 2001,
+    "formats": [
+      "CS",
+      "CD",
+      "LP"],
+    "gold": false
+  }
+  
+  
+  // Add record here
+];
