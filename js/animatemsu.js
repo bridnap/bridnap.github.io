280d5e8810870a9990e605e39cf0e7b32da38a7f
Brian Plunkett <brian.plunkett@gmail.com>
2018-04-03 20:33:50 -0500

    Create animatemsu.js

---
+++ js/animatemsu.js
@@ -0,0 +1,9 @@
+onload = function startAnimation() {
+  var frames = document.getElementById("animation").children;
+  var frameCount = frames.length;
+  var i = 0;
+  setInterval(function() {
+    frames[i % frameCount].style.display = "none";
+    frames[++i % frameCount].style.display = "block";
+  }, 100);
+};
